2d00d595cf38e5577df0628a338deba634cb43b5
Hilel Elizarov <hilel1011@gmail.com>
2022-07-02 21:25:37 +0300

    Added data page
    Refactored dom helper, added methods,
    other minor changes

--- js/data.page.js
+++ js/data.page.js
@@ -1,8 +1,9 @@
-import { getDocs } from "firebase/firestore";
-import { insertTextToElement } from "./dom.helper";
-import { getCollectionRef } from "./fire";
+import { collection, doc, getDocs, onSnapshot } from "firebase/firestore";
+import { appendChildToElementById, appendChildToElementBySelectorOrElem, commonIds, insertTextToElement } from "./dom.helper";
+import { db, getCollectionRef } from "./fire";
 
 showDataByUrlParams();
+
 
 async function showDataByUrlParams() {    
     const urlParams = new URLSearchParams(location.search);
@@ -14,21 +15,89 @@ async function showDataByUrlParams() {
         if (splitted.length > 0) {
             const collectionName = splitted[0];
             const docs = await getCollectionDocsSnapshot(collectionName);
+
+            console.log('----------------------');
+            console.log(docs);
+
+            if (docs.length) {
+
+                const allHeaders = new Set();// Set only unique values
+                docs.forEach(d => {
+                    const docKeys = Object.keys(d);
+                    console.log('docKeys: ', docKeys);
+                    allHeaders.add(...docKeys);
+                });
+
+                [...allHeaders].forEach(h => {
+                    const thElem = document.createElement('TH');
+                    const trSelector = `#${commonIds.dataTable} thead tr`;
+                    console.log(trSelector);
+                    const newChildContent = h;
+                    appendChildToElementBySelectorOrElem(trSelector, thElem, newChildContent);
+                });
+
+                docs.forEach(d => {
+                    const docKeys = Object.keys(d);
+
+                    const trElem = document.createElement('TR');
+                    const tbodySelector = `#${commonIds.dataTable} tbody`;
+                    console.log(tbodySelector);
+                    appendChildToElementBySelectorOrElem(tbodySelector, trElem, null);
+                    
+                    docKeys.forEach(k => {
+                        const tdElem = document.createElement('TD');
+                        // const trSelector = `#${commonIds.dataTable} tbody tr`;// find needed tr by index ??
+                        console.log(trElem);
+                        const newChildContent = d[k];
+                        appendChildToElementBySelectorOrElem(trElem, tdElem, newChildContent);
+                    });
+                });
+            }
             
-            debugger;
-            
-            insertTextToElement('data-json', JSON.stringify(docs));
+            // TODO: on changes need to update page
+            listenToFirestoreChnagesByCollectionNameOrPath(collectionName);s
         }
     }
 }
 
-async function getCollectionDocsSnapshot(name) {
-    const collectionRef = getCollectionRef(name);
+async function getCollectionDocsSnapshot(collName) {
+    const collectionRef = getCollectionRef(collName);
     const querySnapshot = await getDocs(collectionRef);
     const docs = [];
     querySnapshot.forEach(doc => {
         docs.push(doc.data());
     });
-    debugger;
+    console.log(querySnapshot);
     return docs;
 }
+
+let unsubscribeFireChanges;
+// TODO: make separate method for doc and path, this is only for collections
+function listenToFirestoreChnagesByCollectionNameOrPath(collNameOrPathToDoc) {
+    if (!collNameOrPathToDoc) {
+        return;
+    }
+
+    const collRef = collection(db, collNameOrPathToDoc);
+    unsubscribeFireChanges = onSnapshot(
+        collRef,
+        (collection) => {
+            // TODO: on changes need to update page
+            console.log("Current data: ", collection.docs.map(d => d.data()));
+        }
+    );
+}
+
+
+// TODO: check why not working
+window.addEventListener('beforeunload', () => {
+    alert(';');
+    typeof unsubscribeFireChanges === 'function' && unsubscribeFireChanges();
+    console.log(
+        (
+            typeof unsubscribeFireChanges === 'function' && unsubscribeFireChanges()
+        )
+        ? 'unsubscribed'
+        : 'not unsubscribed'
+    );
+});
